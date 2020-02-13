8799b1232414f3358fee90324b32936a117177b4
Priyanshu Tiwari <54521023+PriyanshuMay@users.noreply.github.com>
2020-02-13 19:51:01 +0530

    Create Question1.java
    Complete the code segment to call the method  print() of class Student.

---
+++ Introduction to JAVA/Week 2/Question1.java
@@ -0,0 +1,12 @@
+class Student { 
+    public void print() { 
+		System.out.println("Hi! I am class STUDENT."); 
+    } 
+}
+
+public class Question21{ 
+    public static void main(String args[]){
+    Student s = new Student();
+    s.print();
+         } 
+}
