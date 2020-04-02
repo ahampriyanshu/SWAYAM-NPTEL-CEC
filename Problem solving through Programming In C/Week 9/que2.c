a5c0ebcf5040221a3a20d3511162f2cfcb1ec32c
priyanshumay <tiwarimay2002@gmail.com>
2020-04-02 21:42:52 +0530

    bugs fixed

--- Problem solving through Programming In C/Week 9/que2.c
+++ Problem solving through Programming In C/Week 9/que2.c
@@ -1,23 +1,4 @@
-#include <stdio.h>
- 
-long linear_search(long [], long, long);
- 
-int main()
-{
-   long array[100], search, c, n, position;
- 
-   printf("Input number of elements in array\n");
-   scanf("%ld", &n);
- 
-   printf("Input %d numbers\n", n);
- 
-   for (c = 0; c < n; c++)
-      scanf("%ld", &array[c]);
- 
-   printf("Input a number to search\n");
-   scanf("%ld", &search);
- 
-   position = linear_search(array, n, search);
+position = linear_search(array, n, search);
  
    if (position == -1)
       printf("%d isn't present in the array.\n", search);
@@ -27,8 +8,8 @@ int main()
    return 0;
 }
  
-long linear_search(long a[], long n, long find) {
-   long c;
+int linear_search(int a[], int n, int find) {
+   int c;
  
    for (c = 0 ;c < n ; c++ ) {
       if (a[c] == find)
--- Problem solving through Programming In C/Week 9/que3.c
+++ Problem solving through Programming In C/Week 9/que3.c
@@ -1,22 +1,6 @@
-#include <stdio.h>
-int main()
-{
-  int c, first, last, middle, n, search, array[100];
-
-  printf("Enter number of elements\n");
-  scanf("%d", &n);
-
-  printf("Enter %d integers\n", n);
-
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
-
-  printf("Enter value to find\n");
-  scanf("%d", &search);
-
-  first = 0;
-  last = n - 1;
-  middle = (first+last)/2;
+ int first = 0;
+ int last = n - 1;
+ int middle = (first+last)/2;
 
   while (first <= last) {
     if (array[middle] < search)
