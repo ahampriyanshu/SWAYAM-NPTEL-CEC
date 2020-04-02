61ab92b3f515ee1c882951d5cf67b47ca10c3e9d
priyanshumay <tiwarimay2002@gmail.com>
2020-04-02 22:19:17 +0530

    nptel's compiler is full of shit

--- Problem solving through Programming In C/Week 9/que2.c
+++ Problem solving through Programming In C/Week 9/que2.c
@@ -1,20 +1,33 @@
-position = linear_search(array, n, search);
- 
-   if (position == -1)
-      printf("%d isn't present in the array.\n", search);
+#include <stdio.h>
+int linear_search(int[], int, int);
+int main()
+{
+   int array[100], search, c, n, position;
+   /* search - element to search, c - counter, n - number of elements in array,
+   position - The position in which the element is first found in the list. */
+
+    scanf("%d", &n); // Number of elements in the array is read from the test case data
+
+    for (c = 0; c < n; c++)
+    scanf("%d", &array[c]); //Elements of array is read from the test data
+
+    scanf("%d", &search); 
+ position = linear_search(array, n, search);
+if (position == -1)
+     printf("%d is not present in the array.\n", search);
    else
-      printf("%d is present at location %d.\n", search, position+1);
- 
-   return 0;
+     printf("%d is present at location %d.\n", search, position+1);
+return 0;
 }
  
 int linear_search(int a[], int n, int find) {
    int c;
- 
-   for (c = 0 ;c < n ; c++ ) {
+   for (c = 0 ;c < n ; c++ ) 
+   {
       if (a[c] == find)
          return c;
    }
  
    return -1;
 }
+
